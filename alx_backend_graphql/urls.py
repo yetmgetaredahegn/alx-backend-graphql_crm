17019eec16707c9c29b40ab6575e3c66cc6f23df
Yetmgeta Redahegn <yetmgeta.tech@gmail.com>
2025-10-28 21:03:39 +0300

    initial

--- alx_backend_graphql/urls.py
+++ alx_backend_graphql/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for alx_backend_graphql_crm project.
+URL configuration for alx_backend_graphql project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
