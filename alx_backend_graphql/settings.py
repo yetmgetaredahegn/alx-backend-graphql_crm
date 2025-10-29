f5180861df6d623049ee7ad4d97361422e1dba8a
Yetmgeta Redahegn <yetmgeta.tech@gmail.com>
2025-10-29 16:02:23 +0300

    initial

--- alx_backend_graphql/settings.py
+++ alx_backend_graphql/settings.py
@@ -123,3 +123,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+GRAPHENE = {
+    "SCHEMA": "alx_backend_graphql.schema.schema"
+}
