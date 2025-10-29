c956e49fc5b8cac58e22f3a9644e089dddccf424
Yetmgeta Redahegn <yetmgeta.tech@gmail.com>
2025-10-29 16:21:16 +0300

    graphql

--- alx_backend_graphql/urls.py
+++ alx_backend_graphql/urls.py
@@ -23,6 +23,6 @@ from graphene_django.views import GraphQLView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True)), schema=schema),
+    path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
 ]
 
